77a4e8ea84ded5b9ded6c017e6359fdfde80b899
Shashank Singh <62293640+ssrajputtheboss@users.noreply.github.com>
2021-07-19 13:45:35 +0530

    finally completed && UI updated && added electron-builder and setup for windows

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,ipcMain } = require('electron')
 function createWindow () {
     const win = new BrowserWindow({
       width: 225,
-      height: 135,
+      height: 150,
+      icon:__dirname+'/icon/camera.ico',
       webPreferences:{
         nodeIntegration: true,
         contextIsolation: false,
@@ -11,8 +12,15 @@ function createWindow () {
     })
   
     win.loadFile('index.html')
+    win.removeMenu()
 }
 
 app.whenReady().then(() => {
     createWindow()
+})
+
+app.on('window-all-closed',()=>app.quit())
+
+ipcMain.on('save-close',(e,a)=>{
+  setTimeout(()=>app.quit(),2000)
 })
--- render.js
+++ render.js
@@ -1,28 +1,51 @@
 let recorder = null
-const {spawn,execFile} = require('child_process')
+let m=0,s=0,ele = document.getElementById('timer'),interval=null,head=document.getElementById("head")
+const {execFile} = require('child_process')
+const { ipcRenderer } = require('electron')
 const fs = require('fs')
 function startRecording() {
     if(recorder){
         alert('already record running')
     }else{
         const fname = document.getElementById('fname').value
-        if(!fname)return alert('Fileaname is empty')
-        //document.getElementById('start').setAttribute('disabled',true)
-        //document.getElementById('stop').setAttribute('disabled',false)
-        //if(fs.existsSync('C:/Users/shash/Desktop/recordings'))
-        //    fs.mkdirSync('C:/Users/shash/Desktop/recordings')
-        recorder = execFile('ffmpeg',['-f','gdigrab','-framerate','30','-i','output.mkv'])
+        if(!fname || fname[fname.length-1].match(/\W/))return alert('Fileaname is empty or \nends with a special character')
+        recorder = execFile('ffmpeg',['-f','gdigrab','-framerate','30','-i','desktop',`${fname}.mkv`])
+        document.getElementById('start').disabled=true
+        document.getElementById('stop').disabled=false
+        ele.style.display = 'block'
+        head.style.display = 'none'
+        startTimer()
     }
 }
 
 function stopRecording() {
     if(recorder){
         recorder.kill('SIGINT')
-        console.log(recorder.killed)
-        //document.getElementById('start').setAttribute('disabled',false)
-        //document.getElementById('stop').setAttribute('disabled',true)
-        //recorder = null
+        document.getElementById('start').disabled=false
+        document.getElementById('stop').disabled=true
+        recorder = null
+        ele.style.display = 'none'
+        head.style.display = 'flex'
+        stopTimer()
+        ipcRenderer.send('save-close')
     }else{
         alert('No Recording running')
     }
+}
+
+function startTimer(){
+    interval = setInterval(timer, 1000)
+}
+
+function timer(){
+    if(s===59)
+        m++ 
+    s = (s+1)%60
+    ele.innerHTML = `${m}:${s}`
+}
+
+function stopTimer() {
+    s=0
+    m=0
+    clearInterval(interval)
 }
